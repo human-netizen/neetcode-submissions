0d6707ab12b293028be0bdc494ae7fd30fc402ee
NILOY DAS <81455126+human-netizen@users.noreply.github.com>
2026-05-28 21:06:03 +0600

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minBuy = prices[0];
+        int ans = 0;
+        for(int i = 1 ; i < prices.size() ; i++){
+            ans = max(ans , prices[i] - minBuy);
+            minBuy = min(minBuy , prices[i]);
+        }
+        return ans;
+    }
+};
