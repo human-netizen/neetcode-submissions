aa4ec0da84a1815f0d3008cc387d18a4f17ea269
NILOY DAS <81455126+human-netizen@users.noreply.github.com>
2026-05-28 23:59:11 +0600

    Add: subtree-of-a-binary-tree - submission-4

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-4.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    
+    bool isSub(TreeNode* temp ,TreeNode* subRoot){
+        if(subRoot == NULL && temp == NULL)return 1;
+        if(temp == NULL && subRoot != NULL)return 0;
+        if(temp != NULL && subRoot == NULL)return 0;
+        if(temp->val != subRoot->val)return 0;
+        bool lefti = isSub(temp->left , subRoot->left);
+        bool righti = isSub(temp->right , subRoot->right);
+        return (righti && lefti);
+    }
+    bool parbe(TreeNode *root , TreeNode* subRoot){
+        if(isSub(root , subRoot))return 1;
+        if(root == NULL)return 0;
+        bool one = parbe(root->left , subRoot);
+        bool two = parbe(root->right , subRoot);
+        return (one | two);
+    }
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        
+        return parbe(root , subRoot);
+        
+    }
+};
