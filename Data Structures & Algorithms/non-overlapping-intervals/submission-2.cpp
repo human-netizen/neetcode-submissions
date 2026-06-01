3c22721a37026cfefbc0ba49ab775335c47242f0
NILOY DAS <81455126+human-netizen@users.noreply.github.com>
2026-06-02 00:23:34 +0600

    Add: non-overlapping-intervals - submission-2

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    static int cmp(vector<int>&a , vector<int>&b){
+        if(a[1] == b[1]) return a[0] < b[0];
+        return a[1] < b[1];
+    }
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin() , intervals.end() , cmp);
+        int lastOne = -1000000;
+        int cnt = 0;
+        for(int i = 0 ; i < intervals.size() ; i++){
+            cout << intervals[i][0] << " " << intervals[i][1] << endl;
+            if(intervals[i][0] >= lastOne){
+                lastOne = intervals[i][1];
+                cnt++;
+            }
+        }
+        return intervals.size() - cnt;
+    }
+};
