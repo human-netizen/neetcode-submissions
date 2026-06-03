51f855f56660c81cb5f0ec13379dac0447cb8854
NILOY DAS <81455126+human-netizen@users.noreply.github.com>
2026-06-04 00:39:47 +0600

    Add: search-for-word - submission-5

---
+++ Data Structures & Algorithms/search-for-word/submission-5.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int ro;
+    int co;
+    
+    bool exist(vector<vector<char>>& board, string word) {
+        ro = board.size();
+        co = board[0].size();
+        auto dfs = [&](this auto self , int x , int y , int len){
+            if(len == word.size())return 1;
+            if(x < 0 || y < 0 || x >= ro || y >= co)return 0;
+            if(board[x][y] != word[len])return 0;
+            board[x][y] = '*';
+            int one = self(x + 1 , y , len + 1);
+            int two = self(x , y + 1 , len + 1);
+            int three = self(x - 1 , y , len + 1);
+            int four = self(x , y - 1 , len + 1);
+            board[x][y] = word[len];
+            return (one || two || three || four);
+
+        };
+        for(int i = 0 ; i < ro ; i++){
+            for(int j = 0 ; j < co ; j++){
+                int sol = dfs(i , j , 0);
+                if(sol == 1)return 1;
+            }
+        }
+        return 0;
+    }
+};
