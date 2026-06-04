a688ea2e1ae9432b73b198026f1d762ff1a7884a
NILOY DAS <81455126+human-netizen@users.noreply.github.com>
2026-06-04 12:50:23 +0600

    Add: combination-target-sum - submission-3

---
+++ Data Structures & Algorithms/combination-target-sum/submission-3.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        vector<int>cur;
+        vector<vector<int>> ans;
+        auto solver = [&](this auto self , int i , int rem) ->void {
+            if(rem == 0){
+                ans.push_back(cur);
+                return;
+            }
+            for(int j = i ; j < nums.size() ; j++){
+                if(rem - nums[j] < 0)break;
+                cur.push_back(nums[j]);
+                self(j , rem - nums[j]);
+                cur.pop_back();
+            }
+        }    ;
+        sort(nums.begin() , nums.end());
+        solver(0 , target);
+        return ans;
+    }
+};
