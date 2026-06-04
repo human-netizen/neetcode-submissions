f29935df20cf5363effb760192f1135c2770e23c
NILOY DAS <81455126+human-netizen@users.noreply.github.com>
2026-06-04 16:14:44 +0600

    Add: meeting-schedule-ii - submission-8

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-8.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        if(intervals.empty())return 0;
+        sort(intervals.begin() , intervals.end() , 
+        [](Interval& one , Interval& two){
+            if(one.end == two.end)return one.start < two.start;
+            return one.end < two.end;
+        });
+        multiset<int>st;
+        st.insert(intervals[0].end);
+        for(int i = 1 ; i < intervals.size() ; i++){
+            cout << "start: \n";
+            for(auto s : st)cout << s << " ";
+            cout << endl;
+            auto it = st.upper_bound(intervals[i].start);
+            if(it != st.begin()){
+                it--;
+                st.erase(it);
+            }
+            st.insert(intervals[i].end);
+            for(auto s : st)cout << s << " ";
+            cout << endl;
+        }
+        return st.size() ;
+    }
+};
