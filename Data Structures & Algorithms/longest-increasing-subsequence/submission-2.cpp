da5efd4fbfa681d8f6786a21929c505d303e75af
NILOY DAS <81455126+human-netizen@users.noreply.github.com>
2026-06-04 13:11:29 +0600

    Add: longest-increasing-subsequence - submission-2

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-2.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        set<int>st;
+        
+        for(int num : nums){
+            auto up = st.lower_bound(num);
+            if(up != st.end())st.erase(up);
+            st.insert(num);
+        }
+        return st.size();
+    }
+};
