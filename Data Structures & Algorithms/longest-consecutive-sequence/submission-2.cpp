356eb308306460a3faf0c58b865393278b36242c
NILOY DAS <81455126+human-netizen@users.noreply.github.com>
2026-06-06 00:21:26 +0600

    Add: longest-consecutive-sequence - submission-2

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_map<int,int> up;
+        int ans = 1;
+        for(int i = nums.size() - 1 ; i >= 0 ; i--){
+            up[nums[i]] = 1;
+        }
+        for(auto [first , second] : up){
+            if(second == 1){
+                int cnt = 0;
+                while(up[first] == 1){
+                    up[first] = 0;
+                    first++;
+                    cnt++;
+                }
+                ans = max(ans , cnt);
+            }
+        }
+        return ans;
+    }
+};
