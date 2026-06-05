1039751bd705f2f283c59efa64ee5061dcd4bb2e
NILOY DAS <81455126+human-netizen@users.noreply.github.com>
2026-06-05 11:06:48 +0600

    Add: meeting-schedule - submission-2

---
+++ Data Structures & Algorithms/meeting-schedule/submission-2.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        sort(intervals.begin() , intervals.end() , 
+        [](Interval one , Interval two){
+            if(one.start == two.start)return one.end < two.end;
+            return one.start < two.start;
+        });
+        for(int i = 1 ; i < intervals.size() ; i++){
+            if(intervals[i].end < intervals[i - 1].start)return 0;
+        }
+        return 1;
+    }
+};
