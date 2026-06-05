e68f8299597cacb5142fc3bd0a8da636e86c9392
NILOY DAS <81455126+human-netizen@users.noreply.github.com>
2026-06-05 12:46:16 +0600

    Add: count-number-of-islands - submission-0

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        
+        int ro = grid.size();
+        if(ro == 0)return 0;
+        int co = grid[0].size();
+        auto dfs = [&](this auto self , int i , int j)-> void{
+            if(i < 0 || j < 0 || i >= ro || j >= co)return;
+            if(grid[i][j] == '0')return;
+            grid[i][j] = '0';
+            dfs(i + 1 , j);
+            dfs(i , j + 1);
+            dfs(i - 1 , j);
+            dfs(i + 1  , j);
+        }   
+        int cnt = 0;
+        for(int i = 0 ; i < ro ; i++){
+            for(int j = 0 ; j < co ; j++){
+                if(grid[i][j] == '1'){
+                    cnt++;
+                    dfs(i  , j);
+                }
+            }
+        }
+        return cnt;
+    }
+};
