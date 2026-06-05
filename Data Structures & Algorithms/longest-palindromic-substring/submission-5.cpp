a3be459543489ba3d06242b223fe4cf5a8a7013d
NILOY DAS <81455126+human-netizen@users.noreply.github.com>
2026-06-05 13:19:55 +0600

    Add: longest-palindromic-substring - submission-5

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-5.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = s.size();
+        auto palinTander = [&](this auto self , int i , int j) -> int{
+            if(i < 0 || j >= s.size())return 0;
+            if(s[i] != s[j])return 0;
+            return (i == j ? 1 : 2) + self(i - 1 , j + 1);
+        };
+        int ans = 0;
+        int curI = 0;
+        for(int i = 0 ; i < n ; i++){
+            int one = palinTander(i , i);
+            if(one > ans){
+                ans = one;
+                curI = i;
+            }
+            if(i != n - 1){
+                int two = palinTander(i , i + 1);
+                if(two > ans){
+                    ans = two;
+                    curI = i;
+                }   
+            }
+        }
+        cout << curI << " " << ans << endl;
+        string sub = s.substr(curI - ans / 2 , ans);
+        
+        return sub;
+    }
+};
