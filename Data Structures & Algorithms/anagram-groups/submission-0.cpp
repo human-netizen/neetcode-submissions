b622c5ed3ba4635112f2d18d5fbd9a15184ada68
NILOY DAS <81455126+human-netizen@users.noreply.github.com>
2026-08-08 12:01:18 +0600

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> res;
+        for (const auto& s : strs) {
+            vector<int> cnt(26, 0);
+            for (char c : s) {
+                count[c - 'a']++;
+            }
+            string key = to_string(cnt[0]);
+            for (int i = 1; i < 26; ++i) {
+                key += ',' + to_string(count[i]);
+            }
+            res[key].push_back(s);
+        }
+        vector<vector<string>> result;
+        for (const auto& pair : res) {
+            result.push_back(pair.second);
+        }
+        return result;
+    }
