d9aa24efb0cce09efb6b373961e1376e4f9971e2
NILOY DAS <81455126+human-netizen@users.noreply.github.com>
2026-08-08 12:12:11 +0600

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        for(int i = 0 ; i < (1 << nums.size()) ; i++){
+            vector<int>sol;
+            for(int j = 0 ; j < nums.size() ; j++){
+                if((i >> j) & 1)sol.push_back(nums[j]);
+            }
+            ans.push_back(sol);
+        }
+        return ans;
+    }
+};
