7d648e05260070542da72b41d9053aa1bc787d21
NILOY DAS <81455126+human-netizen@users.noreply.github.com>
2026-09-22 10:34:18 +0600

    Add: lru-cache - submission-5

---
+++ Data Structures & Algorithms/lru-cache/submission-5.cpp
@@ -0,0 +1,35 @@
+class LRUCache {
+public:
+    unordered_map<int,int> cnt , mp;
+    queue<int>q;
+    int capacity;
+    int curSize;
+    LRUCache(int capacity) {
+        this->capacity = capacity;
+        curSize = 0;
+    }
+    
+    int get(int key) {
+        if(cnt[key] != 0)return mp[key];
+        return -1;
+    }
+    
+    void put(int key, int value) {
+        mp[key] = value;
+        cnt[key]++;
+        q.push(key);
+        if(cnt[key] == 1){
+            curSize++;
+        }
+        while(curSize > capacity){
+            int top = q.front();
+            q.pop();
+            cout << top << " " << curSize << " " << capacity << endl;
+            cnt[top]--;
+            if(cnt[top] == 0){
+                curSize--;
+                mp.erase(top);
+            }
+        }
+    }
+};
